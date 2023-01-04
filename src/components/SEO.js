9161f6942e986d2fb2a7a9332d7781c0cc9ee75b
TarasKing <ivanovtaras@live.ru>
2023-01-03 20:41:07 -0800

    #3 SEO, added all pages

---
+++ src/components/SEO.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
+
+const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`;
+
+const SEO = ({ title, description }) => {
+  const { site } = useStaticQuery(query);
+  const metaDescription = description || site.siteMetadata.description;
+  return (
+    <Helmet
+      htmlAttributes={{ lang: "en" }}
+      title={`${title} | ${site.siteMetadata.title}`}
+      meta={[{ name: `description`, content: metaDescription }]}
+      link={[{ rel: "icon", type: "ico", href: "./static/favicon.ico" }]}
+    ></Helmet>
+  );
+};
+
+export default SEO;
--- src/pages/tags.js
+++ src/pages/tags.js
@@ -2,6 +2,8 @@ import React from "react";
 import Layout from "../components/Layout";
 import { graphql, Link } from "gatsby";
 import setupTags from "../utils/setupTags";
+import slugify from "slugify";
+import SEO from "../components/SEO";
 
 const Tags = ({ data }) => {
   // console.log(data);
@@ -9,12 +11,14 @@ const Tags = ({ data }) => {
 
   return (
     <Layout>
+      <SEO title="Tags" />
       <main className="page">
         <section className="tags-page">
           {newTags.map((tag, index) => {
             const [text, value] = tag;
+            const slug = slugify(text, { lower: true });
             return (
-              <Link key={index} to={`/${text}`} className="tag">
+              <Link key={index} to={`/tags/${slug}`} className="tag">
                 <h5>{text}</h5>
                 <p>{value} recipe</p>
               </Link>
